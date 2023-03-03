7f949a778cfb64d20e5c99e99d1e76a9c30138fa
Priscila188 <124712243+Priscila188@users.noreply.github.com>
2023-03-02 23:21:39 -0300

    Trabajo-Clase11

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,17 +1,81 @@
-var Nombre = "Priscila";
-var Apellido = "Pisano";
+let helado = 200;
+let topping;
+let precio;
+let precioFinal;
 
-console.log(Nombre + " " + Apellido);
+topping = "Oreo"
+
+if(topping == "Oreo") {
+    precio = 10;
+} else if (topping == "KitKat") {
+    precio = 15;
+} else if (topping == "Kinder") {
+    precio = 25;
+} else {
+    console.log("No tenemos este topping");
+}
+
+precioFinal = helado + precio;
+
+console.log("El helado cuesta $" + precioFinal)
 
 
-let Número1 = 20;
-let Número2 = 30;
-let Suma = Número1 +Número2;
 
-console.log(Suma);
 
-let Número3 = 40;
-let Suma1 = Suma + Número3;
 
-console.log(Suma1);
 
+
+let Pedido = "verdura";
+
+switch (Pedido) {
+    case 'carne':
+    console.log("Bebida-Vino Tinto");
+    break;
+
+    case 'pescado':
+    console.log("Bebida-Vino Blanco");
+    break;
+
+    case 'verdura':
+    console.log("Bebida-Agua")
+    break;
+
+    default:
+    console.log("Eliga carne, pescado o verdura ")
+
+}
+
+
+
+
+
+
+
+let pedido = ["Carne", "Pescado", "Verdura"];
+
+for (let i = 0; i < pedido.length; i++) {
+    console.log(pedido[i]);
+}
+
+let Verdura = {
+    nombre: "lechuga",
+    color: "verde claro",
+}
+
+pedido.push(Verdura);
+
+for (let i = 0; i < pedido.length; i++) {
+    console.log(pedido[i]);
+}
+
+
+
+
+
+
+let numero = 10 ;
+
+while (numero < 11) {
+    console.log(numero + "es más chico que 11");
+    numero++;
+}
