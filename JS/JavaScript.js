e79aac5efad89383ec26289c98c5f564c33f6f7f
Priscila188 <124712243+Priscila188@users.noreply.github.com>
2023-03-02 23:20:40 -0300

    Trabajo-Clase10

---
+++ JS/JavaScript.js
@@ -0,0 +1,17 @@
+var Nombre = "Priscila";
+var Apellido = "Pisano";
+
+console.log(Nombre + " " + Apellido);
+
+
+let Número1 = 20;
+let Número2 = 30;
+let Suma = Número1 +Número2;
+
+console.log(Suma);
+
+let Número3 = 40;
+let Suma1 = Suma + Número3;
+
+console.log(Suma1);
+
